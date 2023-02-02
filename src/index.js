dd60229d66224dbb059d1fb0d87b6e987e2965fd
NelsonAraujo93 <nelsonaraujoparedes93@hotmail.com>
2023-02-01 21:27:03 -0500

    Fix webpack folders and scripts

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import addYourScore from '../modules/addYourScore.js';
 import recentScore from '../modules/recentScore.js';
 
 const init = () => {
+  debugger;
   const mainContainer = document.getElementById('dynamic-content');
   recentScore(mainContainer);
   addYourScore(mainContainer);
