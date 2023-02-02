680001bba03490417178c0bf519e29056a6de893
NelsonAraujo93 <nelsonaraujoparedes93@hotmail.com>
2023-02-01 21:28:59 -0500

    Fix Linters

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import addYourScore from '../modules/addYourScore.js';
 import recentScore from '../modules/recentScore.js';
 
 const init = () => {
-  debugger;
   const mainContainer = document.getElementById('dynamic-content');
   recentScore(mainContainer);
   addYourScore(mainContainer);
